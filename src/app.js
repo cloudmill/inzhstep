97197cf69dabbdc033a3326374e91e82e1bb8b93
szatolokin <staszatolokin28@gmail.com>
2021-04-26 13:44:08 +0300

    Start header comp

--- src/app.js
+++ src/app.js
@@ -18,9 +18,9 @@ const MAIN_BREAKPOINT = 1024;
 
 			const slider_swiper = new Swiper(slider_swiper_el[0], {
 				spaceBetween: 20,
-				
+
 				slidesPerView: 'auto',
-				
+
 				breakpoints: {
 					[MAIN_BREAKPOINT]: {
 						spaceBetween: 40,
@@ -28,6 +28,46 @@ const MAIN_BREAKPOINT = 1024;
 				},
 			});
 		});
+	});
+}
+
+// --- --- --- --- ---
+
+
+
+
+
+// --- --- HEADER --- ---
+
+{
+	const SCROLL_UPDATE_INTERVAL = 1000 / 30; // 30 fps
+
+	function scrollHandler() {
+		$(window).off('scroll.header');
+
+		const timeout = setTimeout(() => {
+			if (window.pageYOffset >= 1) {
+				$('.header').addClass('header--scroll');
+			} else {
+				$('.header').removeClass('header--scroll');
+			}
+
+			$(window).on('scroll.header', scrollHandler);
+
+			clearTimeout(timeout);
+		}, SCROLL_UPDATE_INTERVAL);
+	}
+
+	$(() => {
+		const header = $('.header');
+
+		if (header.length !== 0) {
+			if (window.pageYOffset >= 1) {
+				header.addClass('header--scroll');
+			}
+
+			$(window).on('scroll.header', scrollHandler);
+		}
 	});
 }
 
