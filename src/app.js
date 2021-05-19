5eab39eb88a688b78e1019e63d8c5d716ef897bd
szatolokin <staszatolokin28@gmail.com>
2021-05-19 19:17:37 +0300

    Update js
    Map

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ const MAIN_BREAKPOINT = 1024;
 				const thumb = slider.find('.product__slider-thumb');
 				thumb.on('click', function () {
 					const index = $(this).index();
-					
+
 					swiper.slideTo(index);
 				});
 			}
@@ -117,6 +117,143 @@ const MAIN_BREAKPOINT = 1024;
 			event.preventDefault();
 
 			$.fancybox.open($('.modal--request'));
+		});
+	});
+}
+
+// map
+{
+	ymaps.ready(() => {
+		// vars
+		const markWidth = 42;
+		const markHeight = 53;
+
+		// init
+		const map = new ymaps.Map('map', {
+			center: [55.76, 37.64],
+			zoom: 10,
+			controls: [],
+		});
+		const zoomControl = new ymaps.control.ZoomControl({
+			options: {
+				size: 'small',
+				position: {
+					top: 205,
+					right: 10,
+				},
+			}
+		});
+
+		// adaptive
+		const mediaQuery = window.matchMedia(`(min-width: ${MAIN_BREAKPOINT}px)`);
+		function mediaQueryChange() {
+			if (mediaQuery.matches) {
+				// desktop
+				map.controls.add(zoomControl);
+			} else {
+				// mobile
+				map.controls.remove(zoomControl);
+			}
+		}
+		mediaQueryChange();
+		mediaQuery.addListener(mediaQueryChange);
+
+		// balloon layout
+		const layout = ymaps.templateLayoutFactory.createClass(
+			[
+				'<div class="balloon">',
+					'<div class="balloon__content">',
+						'<p class="balloon__text">',
+							'{{properties.balloon}}',
+						'</p>',
+					'</div>',
+					'<div class="balloon__arrow"></div>',
+				'</div>',
+			].join(''),
+			{
+				build: function () {
+					this.constructor.superclass.build.call(this);
+	
+					this._$element = $('.balloon', this.getParentElement());
+	
+					this.applyElementOffset();
+				},
+				onSublayoutSizeChange: function () {
+					layout.superclass.onSublayoutSizeChange.apply(this, arguments);
+	
+					if (!this._isElement(this._$element)) {
+						return;
+					}
+	
+					this.applyElementOffset();
+	
+					this.events.fire('shapechange');
+				},
+				applyElementOffset: function () {
+					this._$element.css({
+						left: -(this._$element[0].offsetWidth / 2),
+						top: -(this._$element[0].offsetHeight + markHeight),
+					});
+				},
+				getShape: function () {
+					if (!this._isElement(this._$element)) {
+						return layout.superclass.getShape.call(this);
+					}
+	
+					var position = this._$element.position();
+	
+					return new ymaps.shape.Rectangle(new ymaps.geometry.pixel.Rectangle([
+						[position.left, position.top], [
+							position.left + this._$element[0].offsetWidth,
+							position.top + this._$element[0].offsetHeight,
+						]
+					]));
+				},
+				_isElement: function (element) {
+					return element && element[0];
+				}
+			}
+		);
+
+		// balloon close
+		map.events.add('click', () => {
+			if (map.balloon.isOpen()) {
+				map.balloon.close();
+			}
+		});
+
+		// добавление точек
+		const placemarks = new ymaps.GeoObjectCollection();
+		$('.placemarks__item').each(function () {
+			// данные
+			const balloon = $(this).find('.placemarks__balloon').text().trim();
+			const latitude = $(this).find('.placemarks__latitude').text().trim();
+			const longitude = $(this).find('.placemarks__longitude').text().trim();
+
+			// placemark
+			const coordinates = [latitude, longitude];
+			const placemark = new ymaps.Placemark(coordinates, {
+				balloon,
+			}, {
+				iconLayout: 'default#image',
+				iconImageHref: 'assets/images/placemark.svg',
+				iconImageSize: [markWidth, markHeight],
+				iconImageOffset: [-markWidth / 2, -markHeight],
+
+				balloonLayout: layout,
+				balloonPanelMaxMapArea: 0,
+				hideIconOnBalloonOpen: false,
+			});
+
+			placemarks.add(placemark);
+		});
+
+		// добавление на карту
+		map.geoObjects.add(placemarks);
+
+		// позиционирование на точках
+		map.setBounds(placemarks.getBounds(), {
+			zoomMargin: Math.max(markWidth, markHeight),
 		});
 	});
 }
